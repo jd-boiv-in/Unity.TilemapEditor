3e55e0cd5219a27e9f753dcbe71227cda5703f36
Jean-Denis "JD" Boivin <jd@boiv.in>
2025-03-19 17:03:01 -0400

    Access tilemap editor internals + renamed asmdef

--- Editor/AssemblyInfo.cs
+++ Editor/AssemblyInfo.cs
@@ -1,4 +1,5 @@
 using System.Runtime.CompilerServices;
 
+[assembly: InternalsVisibleTo("Editor")]
 [assembly: InternalsVisibleTo("Unity.2D.Tilemap.EditorTests")]
 [assembly: InternalsVisibleTo("Unity.2D.Tilemap.Package.EditorTests")]
